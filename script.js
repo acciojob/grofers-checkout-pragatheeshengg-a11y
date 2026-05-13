8ae06714a31a49f21de35a8dbc7e79c9ff97be14
pragatheeshengg-a11y <pragatheeshengg@gmail.com>
2026-05-13 19:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+let done=0;
 const getSum = () => {
 //Add your code here
+	if(done!=0){/*initial click 1st done is 0 condition fail then done=1 now the 2nd click done is!=0 so return noting */
+        return ;
+    }
+     
+    done=1;
+    let prc=document.querySelectorAll('.price');
+let sum=0;
+for(let i=0;i<prc.length;i++){
+        let temp=prc[i].textContent;
+        sum+=Number(temp);
+}
+/*alert(`The sum is : ${sum}`);*/
+ let tr=document.createElement('tr');
+ let td1=document.createElement('td');
+ let td2=document.createElement('td');
+ td1.textContent="Total";
+ td2.textContent=`${sum}`;
+ tr.appendChild(td1);
+ tr.appendChild(td2);
+ 
+ let table=document.querySelector('table');
+ table.appendChild(tr);
   
 };
 
