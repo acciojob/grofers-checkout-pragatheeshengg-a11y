92a9ffd7e85878b561ba4b9ca8dfbaf2abcb0355
pragatheeshengg-a11y <pragatheeshengg@gmail.com>
2026-05-15 12:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const getSumBtn = document.createElement("button");
+
 getSumBtn.innerText = "Get Total Price";
 
 document.body.appendChild(getSumBtn);
@@ -13,18 +14,18 @@ const getSum = () => {
         sum += Number(price.innerText);
     });
 
-    // Create row
     let tr = document.createElement("tr");
 
-    // Create cell
     let td = document.createElement("td");
 
     td.setAttribute("colspan", "2");
+
+    td.setAttribute("id", "ans");
+
     td.innerText = sum;
 
     tr.appendChild(td);
 
-    // Append to table
     document.querySelector("table").appendChild(tr);
 };
 
